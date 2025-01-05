a59ff97eb5940598c3bafea9610b706a8a427a33
Sathmi1234 <devsadini@gmail.com>
2025-01-05 20:33:28 +0530

    Created the structure of About Us

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,12 +1,24 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 
-export default function AboutUs(){
-    return(
-        <View>
-            <Text>
-                About Us
-            </Text>
-        </View>
-    )
+export default function AboutUs() {
+    return (
+        <ScrollView contentContainerStyle={styles.scrollView}>
+            <View>
+                <Image
+                    source={require('../assets/about_us.jpg')}
+                />
+                <Text variant="headlineLarge">Let's Get to Know</Text>
+                <Divider />
+                <Text variant="bodyMedium">
+                    Hiii
+                    Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
+                    To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.        
+                </Text>
+                <Button mode="outlined">
+                    About Us
+                </Button>
+            </View>
+        </ScrollView>
+    );
 }
