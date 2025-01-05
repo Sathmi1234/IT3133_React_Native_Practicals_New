c9d3309fab875470f8c7b56e4904310ffed4b3dc
Sathmi1234 <devsadini@gmail.com>
2025-01-05 20:11:53 +0530

    Created Home, ContactUs, AboutUs components

---
+++ components/AboutUs.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function AboutUs(){
+    return(
+        <View>
+            <Text>
+                About Us
+            </Text>
+        </View>
+    )
+}
---
+++ components/ContactUs.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export default function ContactUs(){
+    return(
+        <View>
+            <Text>
+                Contact Us
+            </Text>
+        </View>
+    )
+}
