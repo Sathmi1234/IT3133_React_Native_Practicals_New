e416af6cd42f266cc878c20cf32a5354cd7dd52a
Sathmi1234 <devsadini@gmail.com>
2025-01-05 20:02:45 +0530

    Removed previous content

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { StyleSheet, View, ScrollView, KeyboardAvoidingView,Platform } from "react-native";
 import { PaperProvider } from 'react-native-paper';
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import Home from "./components/Home";
-import ContactUs from "./components/ContactUs";
 
 export default function Index() {
   return (
     <PaperProvider>
       <SafeAreaProvider>
         <SafeAreaView>
-          <KeyboardAvoidingView behavior={Platform.OS ==='ios' ? padding:'heights'}>
-            <ScrollView>
-              <View style={styles.container}>
-                <ContactUs />
-                <StatusBar style="auto" />
-              </View>
-            </ScrollView>
-          </KeyboardAvoidingView>
+
         </SafeAreaView>
       </SafeAreaProvider>
     </PaperProvider>
