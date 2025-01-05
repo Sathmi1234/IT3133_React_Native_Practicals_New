13f5e699bb6743a6242966d1ecc1af318b7c8ff2
Sathmi1234 <devsadini@gmail.com>
2025-01-05 20:59:16 +0530

    Added navigation #aboutUs

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,7 +1,9 @@
-import { Image, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import { Button, Divider, Text } from "react-native-paper";
 
 export default function AboutUs() {
+    const navigation =useNavigation();
     return (
         <ScrollView contentContainerStyle={styles.scrollView}>
             <View style={styles.container}>
@@ -16,8 +18,8 @@ export default function AboutUs() {
                     Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
                     To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.        
                 </Text>
-                <Button mode="outlined">
-                    About Us
+                <Button mode="outlined" onPress={() => navigation.push('Contact')}>
+                    Contact Us
                 </Button>
             </View>
         </ScrollView>
@@ -32,7 +34,8 @@ const styles = StyleSheet.create({
     },
     about:{
         width:300,
-        height:300
+        height:300,
+        alignSelf:'center'
     },
     body: {
         padding: 8,
