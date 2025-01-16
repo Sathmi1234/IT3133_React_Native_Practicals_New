17934a0dab0c588e471c79b50361c10ebd9588b1
Sathmi1234 <devsadini@gmail.com>
2025-01-16 13:18:30 +0530

    Modified App.js by adding new screens

--- App.js
+++ App.js
@@ -1,39 +1,36 @@
+import { StyleSheet, View} from "react-native";
+import StudentList from "./components/StudentList";
+import StudentListDb from "./components/StudentListDb";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet} from "react-native";
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider} from "react-native-safe-area-context";
-import Home from "./components/Home";
-import ContactUs from "./components/ContactUs";
-import AboutUs from "./components/AboutUs";
+import Profile from "./components/Profile";
+import AddStudent from "./components/AddStudent";
+import UpdateStudent from "./components/UpdateStudent";
 
 export default function Index() {
-  const Stack = createNativeStackNavigator();
+  const Stack=createNativeStackNavigator();
   return (
-    <PaperProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home" screenOptions={{headerStyle:{backgroundColor:'tomato'}}}>
-            <Stack.Screen name="Home" component={Home}/>
-            <Stack.Screen name="Contact" component={ContactUs}/>
-            <Stack.Screen name="About" component={AboutUs}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </PaperProvider>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="StudentList" component={StudentListDb}/>
+          <Stack.Screen name="Profile" component={Profile}/>
+          <Stack.Screen name="Add" component={AddStudent}/>
+          <Stack.Screen name="UpdateStudent" component={UpdateStudent} />
+
+        </Stack.Navigator>  
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  body: {
-    padding: 5,
-    textAlign: "justify",
-    color: 'black'
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
   }
-});
+})
+
